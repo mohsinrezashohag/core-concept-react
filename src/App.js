5572e2d4b51690251cc335f0e7a09bce79c8848c
mohsinrezashohag <mohsinrezashohag2@gmail.com>
2021-09-19 13:45:35 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -14,36 +15,64 @@ function App() {
     "programming book",
     "math book",
     "JS books",
+    'Bangla Book'
+  ];
+
+  const cinemas = [
+    { name: "3 idiots", hero: "amir khan" },
+    { name: "tere naam", hero: "salman khan" },
+    { name: "total dhamal", hero: "ritesh" },
+    { name: "barfi", hero: 'ranbeer' }
   ];
 
   return (
     <div className="App">
-      <h6>Similar In Look,Different In Data</h6>
+      <h2 className="section-heading">Similar In Look,Different In Data</h2>
       <div className="player-box">
         <Player name="sakib al hasan" category="all-rounder"></Player>
         <Player name="Tamim Iqbal" category="Batsman"></Player>
         <Player name="Mushfiq" category="Batsman"></Player>
       </div>
 
-      <ul>
+      {/* <ul>
         {books.map((book) => (
           <li>{book}</li>
         ))}
-      </ul>
+      </ul> */}
 
-      <h6>Books [Arrays Data [book name]]</h6>
+      <h2 className="section-heading">Books [Arrays Data [book name]]</h2>
       <div className="books-box">
-        <Books books={books[0]}></Books>
-        <Books books={books[1]}></Books>
-        <Books books={books[2]}></Books>
+
+        {books.map(book => <Books book={book}></Books>)}
       </div>
 
-      <h6>Friends [Arrays Object Data]</h6>
+
+
+
+
+      <h2 className="section-heading">Friends [Arrays Object Data]</h2>
       <div className="friends-box">
         <Friends friend={friends[0]}></Friends>
         <Friends friend={friends[1]}></Friends>
         <Friends friend={friends[2]}></Friends>
       </div>
+
+
+
+
+      <h2 className="section-heading">Cinema dynamic component</h2>
+      <div className="cinema-box">
+        {cinemas.map(cinema => <Cinema cinema={cinema.name} hero={cinema.hero}></Cinema>)}
+      </div>
+
+
+      <h2 className="section-heading">Counter dynamic component</h2>
+      <div className="counter-box">
+        <Counter></Counter>
+      </div>
+
+
+
 
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -71,13 +100,15 @@ function Player(props) {
         backgroundColor: "pink",
         padding: "3px",
         margin: "3px",
-      }}
-    >
+      }}>
       <h3>Hello I'm Player : {props.name}</h3>
       <h4>Category : {props.category}</h4>
     </div>
   );
 }
+
+
+
 
 function Friends(props) {
   const { name, age } = props.friend;
@@ -89,15 +120,16 @@ function Friends(props) {
         backgroundColor: "orange",
         padding: "3px",
         margin: "3px",
-      }}
-    >
+      }}>
       <h3>Friend Name : {name} </h3>
       <h4>Friend Age :{age} </h4>
     </div>
   );
 }
 
+
 function Books(props) {
+
   return (
     <div
       style={{
@@ -105,11 +137,62 @@ function Books(props) {
         backgroundColor: "cyan",
         padding: "3px",
         margin: "3px",
-      }}
-    >
-      <h3>Book Name : {props.books}</h3>
+      }}>
+      <h3>Book Name :{props.book} </h3>
     </div>
   );
 }
+
+
+
+function Cinema(props) {
+  return (
+    <div
+      style={{
+        color: "white",
+        backgroundColor: "blue",
+        padding: "3px",
+        margin: "3px",
+      }}>
+      <h3>cinema name : {props.cinema} </h3>
+      <h4>hero : {props.hero} </h4>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+// Advanced state component and hooks  
+
+
+function Counter(props) {
+  const [count, setCount] = useState(5)
+  console.log(count, setCount);
+  const countHandler = () => setCount(count + 1);
+  const countHandlerMinus = () => {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  };
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+
+      <button onClick={countHandler} >Click to increase</button>
+      <button onClick={countHandlerMinus} >Click to Decrease</button>
+    </div>
+  )
+
+}
+
+
+
+
 
 export default App;
